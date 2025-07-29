ca68f354f2151956b937d5fa5d3b9dba6df763de
KENAKI - MINGUITO,KENNETH JOHN C. <Minguitokennethjohn@gmail.com>
2025-07-29 01:25:26 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ app.use(express.json());
 
 const filePath = "./contact.json";
 
-// Utility functions
 function getContacts() {
   const data = fs.readFileSync(filePath, "utf8");
   return JSON.parse(data);
@@ -16,13 +15,11 @@ function saveContacts(data) {
   fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 }
 
-// GET: Read all contacts
 app.get("/api/contacts", (req, res) => {
   const contacts = getContacts();
   res.json(contacts);
 });
 
-// POST: Add a new contact
 app.post("/api/contacts", (req, res) => {
   const contacts = getContacts();
   const { name, email } = req.body;
@@ -38,7 +35,6 @@ app.post("/api/contacts", (req, res) => {
   res.status(201).json({ message: "Contact added", contact: newContact });
 });
 
-// PUT: Update a contact by index
 app.put("/api/contacts/:index", (req, res) => {
   const index = parseInt(req.params.index);
   const contacts = getContacts();
@@ -58,7 +54,6 @@ app.put("/api/contacts/:index", (req, res) => {
   res.json({ message: "Contact updated", contact: contacts[index] });
 });
 
-// DELETE: Remove contact by index
 app.delete("/api/contacts/:index", (req, res) => {
   const index = parseInt(req.params.index);
   const contacts = getContacts();
@@ -72,7 +67,6 @@ app.delete("/api/contacts/:index", (req, res) => {
   res.json({ message: "Contact deleted", deleted });
 });
 
-// Start the server
 app.listen(5000, () => {
   console.log("🚀 Server running at http://localhost:5000");
 });
